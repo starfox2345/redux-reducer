48b5045f1b4a54ce4ca66c2e167ec5934606454d
yatsun <yatsunlam25@gmail.com>
2021-11-19 01:25:27 -0500

    commit tree c65c9af52caeee3b36861b7ea623a2edfdd7f391

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return ( {...state, friends: [...state.friends, action.friend]} )
+
+        case 'REMOVE_FRIEND':
+            const index = state.friends.findIndex(friend => friend.id === action.id)
+
+            return ( {...state, friends: [...state.friends.slice(0, index), ...state.friends.slice(index + 1)]} )
+        
+        default: return state
+    }
 }
